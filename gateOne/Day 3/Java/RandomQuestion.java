c8dc4ae018fe894eece782fdbd7442a09c0a4b7b
Chukwukaesther <Chukwukaesther747@gmail.com>
2024-05-08 13:50:46 +0100

    Adding up files to git

---
+++ gateOne/Day 3/Java/RandomQuestion.java
@@ -0,0 +1,82 @@
+import java.util.Random;
+import java.util.Scanner;
+public class RandomQuestion{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+
+
+int numberOne = (int) Math.random() + 1;
+int numberTwo = (int) Math.random() + 1;
+int operator = (int) math.random() + 1;
+
+
+int answer = numberOne + numberTwo;
+if (oerator == 1){
+	System.out.print("what is "numberOne" + " numberTwo: ");
+	answer = input.nextInt();
+}
+
+if(answer == numberOne + numberTwo){
+	System.out.print("correct");
+}
+
+else if(numberOne != numberTwo){
+	System.out.print(incorrect);
+}
+
+
+
+int answer = numberOne - numberTwo;
+if (oerator == 1){
+System.out.print("what is "numberOne" + " numberTwo: ");
+answer = input.nextInt();
+}
+
+if(answer == numberOne - numberTwo){
+System.out.print("correct");
+}
+else if(numberOne != numberTwo){
+System.out.print(incorrect);
+}
+
+
+int answer = numberOne / numberTwo;
+if (oerator == 1){
+System.out.print("what is "numberOne" / " numberTwo: ");
+answer = input.nextInt();
+}
+
+if(answer == numberOne - numberTwo){
+System.out.print("correct");
+}
+else if(numberOne != numberTwo){
+System.out.print(incorrect);
+}
+
+
+
+int answer = numberOne * numberTwo;
+if (oerator == 1){
+System.out.print("what is "numberOne" + " numberTwo: ");
+answer = input.nextInt();
+}
+
+if(answer == numberOne * numberTwo){
+System.out.print("correct");
+}
+else if(numberOne != numberTwo){
+System.out.print(incorrect);
+}
+
+
+
+
+
+
+
+
+
+
+ 
+
+
