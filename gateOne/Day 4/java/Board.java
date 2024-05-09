42a3439742be462e7300b16a70450e19af297485
Chukwukaesther <Chukwukaesther747@gmail.com>
2024-05-09 14:27:41 +0100

    task

---
+++ gateOne/Day 4/java/Board.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Board {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		String[][] alphabet = new String[3][3];
+		
+		int rows = alphabet.length;
+		int column = alphabet[0].length;
+
+		System.out.println("Rows: " + rows);
+		System.out.println("Column: " + column);
+		
+
+		for(int digit = 0; digit < 9; digit++) {
+			System.out.print("Enter alpahbet: ");
+			String alphabet = scanner.nextLine();
+		}
+	
+
+		for (int count = 0; count < alphabet.length; count++){
+			for (int counter = 0; counter < alphabet.length; counter++){
+				alphabetTime[count][counter] = diit;
+			}
+		}
+	}
+}
---
+++ gateOne/Day 4/python/Board.java
@@ -0,0 +1,12 @@
+list = []
+for digit in range(0, 9):
+	user_input = input("Enter Alphabet: "):
+	list.append(user_input)
+	
+
+#grid = [[user_input for column in range(3) for row in range(3)]]
+#print(list)
+#for index in range(len(list)):
+	#print(list[index])
+for column in range(3):
+	print(list)
