6ca2472882205d4c8a162e1a1ef9aeba09e550df
Chukwukaesther <Chukwukaesther747@gmail.com>
2024-05-10 13:13:49 +0100

    Adding up files to git

---
+++ gateOne/Day 5/Java/cardValidator.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class CardValidator {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+
+}
+
+		String cardInput = "";
+		int doubleNumber = 0;
+		int sum = 0;
+		int sumTotalNumber = 0
+		String valid;
+		
+
+		System.out.print("Enter Card number");
+		String cardNumber = input.nextLine();
+
+		for(int count = 0; count < 16; count+=2){
+			if(cardNumber[count] % 2 == 0 ){
+			cardNumber = cardInput;
+				doubleNumber += cardNumber[count];
+		
+			if(cardNumber[count] % 2 != 0){
+				sum += cardNumber[count];
+
+				SumTotalNumber = cardNumber + doubleNumber;
+			if(sumNumber / 16);
+
+			
+	
+			}
+			 
+		}
+		return valid	
+		
+
+	}	
+
+
+
+
+
+ 
