19dfbd3105b1c2b6a84f0594ceb09eeabddd3242
Chukwukaesther <Chukwukaesther747@gmail.com>
2024-05-13 15:06:05 +0100

    My work

---
+++ gateOne/Day7/java/PeriodCalculator.java
@@ -0,0 +1,76 @@
+import java.util.Scanner:
+public Class PeriodCalculator{
+	public static void main(String... args){
+		Scanner input = new Scanner(System.in);
+
+
+		System.out.print("PERIOD TRACKER APP");
+		System.out.print("Menstrual circle is a natuaral process. it is a complex circle controlled by female hormones that cause regular bleading(periods)");
+
+		int[] lengthOfCircle ={21,28,35};
+		
+		System.out.print("Enter the date your period started");
+		int userInput = Scanner.nextInt();
+	
+		for(int count = 0; count <= 7; count++){
+			if(lenghtOfCircle != count){
+			
+			
+			
+			
+	
+		}
+
+		
+	}
+		
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
