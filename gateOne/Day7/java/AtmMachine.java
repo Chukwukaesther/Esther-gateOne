92d633cd52a3d291614931139f67acc9b9669e95
Chukwukaesther <Chukwukaesther747@gmail.com>
2024-05-13 21:02:06 +0100

    update

--- gateOne/Day7/java/AtmMachine.java
+++ gateOne/Day7/java/AtmMachine.java
@@ -1,10 +1,84 @@
 import java.util.Scanner;
-public class PeriodCalculator{
+public class AtmMachine{
 	public static void main(String... args){
 		Scanner input = new Scanner(System.in);
 
 
 
 
-System.out.print("Welcome to Atm");
-System.ouy.print("Enter your pin");
+		System.out.println("Welcome to Atm");
+
+
+		System.out.print("Enter your first name: ");
+		String firstName = input.nextLine();
+
+
+		System.out.print("Enter your last name: ");
+		String lastName = input.nextLine();
+
+		
+		System.out.print("Enter your pin: ");
+		int pin = input.nextInt();
+
+		System.out.println("Account created successfully");
+		System.out.println("FirstName: " + firstName);
+		System.out.println("LastName: " + lastName);
+		System.out.println("Pin: " + pin);
+
+
+		System.out.print("Enter your account number");
+		String accountNumber = input.nextLine();
+
+		System.out.print("Enter your pin");
+		int pin = input.nextInt();
+
+		System.out.print("Enter the amount you want to deposit: $");
+		double depositAmount = input.nextDouble();
+
+		if(depositAmount > 0){
+			//deposit(depositAmount);
+			System.out.print("Deposit Successful");
+
+
+		} 
+
+
+		
+
+
+
+		
+
+
+
+
+
+	}
+}
+		
+
+
+		
+
+
+
+		
+
+
+
+		
+
+
+
+		
+
+
+
+		
+
+		
+
+
+		
+
+
