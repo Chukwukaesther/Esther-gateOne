ab7025a83baa24d2af126f3d452ee68bd30afe2e
Chukwukaesther <Chukwukaesther747@gmail.com>
2024-05-13 22:56:18 +0100

    update

--- gateOne/Day7/java/AtmMachine.java
+++ gateOne/Day7/java/AtmMachine.java
@@ -1,13 +1,18 @@
+import java.util.Random;
 import java.util.Scanner;
 public class AtmMachine{
 	public static void main(String... args){
 		Scanner input = new Scanner(System.in);
+		Random random = new Random();
 
 
 
 
 		System.out.println("Welcome to Atm");
 
+		int pin ;
+		int accountNumber;
+		Double balance;
 
 		System.out.print("Enter your first name: ");
 		String firstName = input.nextLine();
@@ -15,10 +20,13 @@ public class AtmMachine{
 
 		System.out.print("Enter your last name: ");
 		String lastName = input.nextLine();
+		
+		accountNumber = random.nextInt(1, 2500);
+		System.out.println("This is your account number: " + accountNumber);
 
 		
 		System.out.print("Enter your pin: ");
-		int pin = input.nextInt();
+		pin = input.nextInt();
 
 		System.out.println("Account created successfully");
 		System.out.println("FirstName: " + firstName);
@@ -26,18 +34,29 @@ public class AtmMachine{
 		System.out.println("Pin: " + pin);
 
 
-		System.out.print("Enter your account number");
-		String accountNumber = input.nextLine();
+		System.out.print("Enter your account number: ");
+		accountNumber = input.nextInt();
 
-		System.out.print("Enter your pin");
-		int pin = input.nextInt();
+		System.out.print("Enter your pin: ");
+		pin = input.nextInt();
 
 		System.out.print("Enter the amount you want to deposit: $");
 		double depositAmount = input.nextDouble();
 
 		if(depositAmount > 0){
-			//deposit(depositAmount);
 			System.out.print("Deposit Successful");
+
+			balance = depositAmount;
+		}
+		
+		System.out.print("How much do you wish to withdraw");
+		int withdrawAmount = nextInt();
+
+		
+		if(balace < amount){
+
+		} 
+
 
 
 		} 
