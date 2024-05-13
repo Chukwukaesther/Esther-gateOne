f74d0c38d553ed9f5dcaf2cb0ebb854f35f9ba6e
Chukwukaesther <Chukwukaesther747@gmail.com>
2024-05-13 19:05:27 +0100

    addgit

--- gateOne/Day7/java/PeriodCalculator.java
+++ gateOne/Day7/java/PeriodCalculator.java
@@ -1,32 +1,34 @@
 import java.time.LocalDate;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 public class PeriodCalculator{
 	public static void main(String... args){
 		Scanner input = new Scanner(System.in);
 
-
 		System.out.print("PERIOD TRACKER APP");
 		System.out.print("Menstrual circle is a natuaral process. it is a complex circle controlled by female hormones that cause regular bleading(periods)");
 
 		System.out.print("Enter the date your period started: dd/MM/yyyy: ");
 		String dateOfPeriod = input.nextLine();
+		
 	}
 
 
 	int lengthOfCircle = 28;
 	int anotherperiodDate = 0;
 
-	public static int periodTracker(String DateOfPeriod){
+	public static int periodTracker(String dateOfPeriod){
 		
 
 		DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		LocalDate anotherDateOfPeriod = LocalDate.parse(dateOfPeriod,format);
 		LocalDate now = LocalDate.now();
+		period anotherPeriodDate = lengthOfCircle + dateOfPeriod;
 		 
 
 	
-	 	int anotherPeriodDate = dateofPeriod + lengthOfCircle;
+
 			
 		return anotherPeriodDate;	
 	
