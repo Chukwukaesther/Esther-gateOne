2dbac39d107da20e74b4993e71396bbc6e69d854
Chukwukaesther <Chukwukaesther747@gmail.com>
2024-05-15 15:13:45 +0100

    update

--- gateOne/Day7/java/PeriodCalculator.java
+++ gateOne/Day7/java/PeriodCalculator.java
@@ -1,28 +1,34 @@
-
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.Scanner;
-public class PeriodCalculater {
+public class PeriodCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 	
-	System.out.print("PERIOD TRACKER APP");
-	System.out.print("Menstrual circle is a natuaral process. it is a complex circle controlled by female hormones that cause regular bleading(periods)");
+
+
+	
+
+	System.out.println("PERIOD TRACKER APP");
+
+	System.out.println("");
+
+	System.out.println("Menstrual circle is a natuaral process. it is a complex circle controlled by female hormones that cause regular bleading(periods).");
+
+	System.out.println("");
+	System.out.println("");
 
         
-        System.out.println("Enter the start date of your menstrual cycle (in dd/mm/yyyy format):");
+        System.out.println("Enter the start date of your menstrual cycle: ");
         String startDate = scanner.nextLine();
-        
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        LocalDate startDate = LocalDate.parse(startDate, formatter);
-        
-        System.out.println("Enter the number of days in your cycle:");
-        int cycleLength = scanner.nextInt();
-        
-        System.out.println("Enter the number of days since the start of your cycle:");
-        int daysSinceStart = scanner.nextInt();
-        
-        
-       
-    }	
+
+
+	System.out.println("Enter the Last date of your menstrual cycle:");
+        String lastDate = scanner.nextLine();
+
+	SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/dd/YYYY, locale.ENGLISH");
+	Date  date1 = sdf.parse("startDate");
+	Date  date2 = sdf.parse("lastDate");
+
+	}
 }
